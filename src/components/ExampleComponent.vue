5dbc6de25e097da2b86f3cfb78165e6cd19dd5e4
Meirmanov <dau.kassymov@aues.kz>
2024-03-16 12:39:26 +0500

    16.02.2024

--- src/components/ExampleComponent.vue
+++ src/components/ExampleComponent.vue
@@ -1,37 +1,88 @@
 <template>
   <div>
-    <p>{{ title }}</p>
-    <ul>
-      <li v-for="todo in todos" :key="todo.id" @click="increment">
-        {{ todo.id }} - {{ todo.content }}
-      </li>
-    </ul>
-    <p>Count: {{ todoCount }} / {{ meta.totalCount }}</p>
-    <p>Active: {{ active ? 'yes' : 'no' }}</p>
-    <p>Clicks on todos: {{ clickCount }}</p>
+    <q-input v-model="name" label="Name"></q-input>
+    <q-input v-model="description" label="Description"></q-input>
+    <q-input v-model="price" label="Price"></q-input>
+    <q-input v-model="tax" label="Tax"></q-input>
+    <q-btn style="width: 100%; margin-top: 10px;" @click="postData" label="Submit"></q-btn>
+
+    <q-table
+      style="margin-top: 20px;"
+      :rows="items"
+      :columns="columns"
+    />
   </div>
 </template>
 
-<script setup lang="ts">
-import { computed, ref } from 'vue';
-import { Todo, Meta } from './models';
+<script>
+import axios from 'axios';
 
-interface Props {
-  title: string;
-  todos?: Todo[];
-  meta: Meta;
-  active: boolean;
-}
-const props = withDefaults(defineProps<Props>(), {
-  todos: () => [],
-});
-
-const clickCount = ref(0);
-function increment() {
-  clickCount.value += 1;
-  return clickCount.value;
-}
-
-const todoCount = computed(() => props.todos.length);
-
+export default {
+  data() {
+    return {
+      name: '',
+      description: '',
+      price: '',
+      tax: '',
+      items: [],
+      columns: [
+        {
+          name: 'name',
+          label: 'Name',
+          align: 'left',
+          field: 'name',
+        },
+        {
+          name: 'description',
+          label: 'Description',
+          align: 'left',
+          field: 'description',
+        },
+        {
+          name: 'price',
+          label: 'Price',
+          align: 'left',
+          field: 'price',
+        },
+        {
+          name: 'tax',
+          label: 'Tax',
+          align: 'left',
+          field: 'tax',
+        }
+      ]
+    };
+  },
+  methods: {
+    async postData() {
+      try {
+        await axios.post('http://127.0.0.1:8000/insert/', {
+          name: this.name,
+          description: this.description,
+          price: this.price,
+          tax: this.tax
+        });
+        this.getItems();
+        // Очистить форму после отправки данных
+        this.name = '';
+        this.description = '';
+        this.price = '';
+        this.tax = '';
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async getItems() {
+      try {
+        const response = await axios.get('http://127.0.0.1:8000/inserted/');
+        this.items = response.data;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  },
+  mounted() {
+    this.getItems();
+  }
+};
 </script>
